f6dba09b2d02fe771f45fc76bd6ba28e9d9326d2
Andrea Francia <andrea@andreafrancia.it>
2011-10-02 19:54:56 +0200

    Refactor.

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -12,14 +12,18 @@ class GetSubs:
     def run(self, showname, season, episode):
         zip_file_contents=self.http.get('/zip-file')
 
-        zip_file = zipfile.ZipFile(StringIO(zip_file_contents), 'r')
-        for name in zip_file.namelist():
-            zipped_file=zip_file.open(name).read()
-            break
-        zip_file.close()
-        
+        subtitles_file=extract_first_zipped_file(zip_file_contents)
+
         self.destination.place_file(named='subtitles.srt',
-                                    contents=zipped_file)
+                                    contents=subtitles_file)
+
+def extract_first_zipped_file(zip_file_contents):
+    zip_file = zipfile.ZipFile(StringIO(zip_file_contents), 'r')
+    for name in zip_file.namelist():
+        zipped_file=zip_file.open(name).read()
+        break
+    zip_file.close()
+    return zipped_file
 
 class HtmlPage:
     def __init__(self, contents):
@@ -31,24 +35,6 @@ class HtmlPage:
 def extract_zip_link(page_contents):
     page=HtmlPage(page_contents)
     return page.xpath('//a[h1/text()="Download"]/@href')[0]
-
-def download_and_unpack_subtitles_file(url, dest_name):
-    zip_file_contents = readurl(url)
-    unpack_all_files_as(zip_file_contents, dest_name)
-
-def unpack_all_files_as(zip_file_contents, dest_name):
-    for zipped_file in all_zipped_file_in(zip_file_contents):
-        with zipped_file, file(dest_name,'w') as dest:
-            shutil.copyfileobj(zipped_file, dest)
-
-def readurl(url):
-    with closing(urlopen(url)) as f:
-        return f.read()
-
-def all_zipped_file_in(zipfile_contents):
-    zipstream = zipfile.ZipFile(StringIO(zipfile_contents))
-    for name in zipstream.namelist():
-        yield zipstream.open(name)
 
 def pick_first_details_link(page_contents):
     page=HtmlPage(page_contents)
--- test_data/__init__.py
+++
@@ -1,6 +0,0 @@
-def load(path):
-    contents = file(resolve(path)).read()
-    return contents
-
-def resolve(path):
-    return 'test_data/' + path
