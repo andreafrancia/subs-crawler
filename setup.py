6e4d05d4ab44fbc5774548b4afe4602f8bed3796
Andrea Francia <andrea@andreafrancia.it>
2011-10-30 15:30:31 +0100

    Split out deps for development from deps for setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distribute_setup import use_setuptools
+use_setuptools()
 from setuptools import setup
 
 setup(
@@ -6,4 +7,5 @@ setup(
     author='Andrea Francia',
     entry_points={ 'console_scripts' : [ 'crawls-subs = crawler:main']},
     py_modules=['crawler'],
+    install_requires = file('requirements.txt').read().split(),
     )
